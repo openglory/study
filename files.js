67c9bb8443b4c22d392bfda0cfe6f2d7fec7dec7
openglory <rr4l813102+github@gmail.com>
2025-02-01 02:18:58 +0530

    added files.js

---
+++ files.js
@@ -0,0 +1,5 @@
+let file_list = [
+    { filepath: "https://cdn-icons-png.flaticon.com/512/3767/3767084.png", title: "File 1", icon: "https://cdn-icons-png.flaticon.com/512/3767/3767084.png" },
+    { filepath: "file_2.html", title: "File 2", icon: "icon2.png" },
+    { filepath: "file_3.html", title: "File 3", icon: "icon3.png" }
+];
