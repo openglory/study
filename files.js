e639aeece9a79447f86a02f052e41001a5495256
openglory <rr4l813102+github@gmail.com>
2025-02-01 02:40:01 +0530

    Update files.js

--- files.js
+++ files.js
@@ -1,5 +1,5 @@
 let file_list = [
-    { filepath: "apps/periodic-table-of-elementshtml-css/index.html", title: "File 1", icon: "https://cdn-icons-png.flaticon.com/512/3767/3767084.png" },
+    { filepath: "apps/periodic-table-of-elementshtml-css/index.html", title: "Priodic table 1", icon: "https://cdn-icons-png.flaticon.com/512/3767/3767084.png" },
     { filepath: "file_2.html", title: "File 2", icon: "icon2.png" },
     { filepath: "file_3.html", title: "File 3", icon: "icon3.png" }
 ];
