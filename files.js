d0114cdc7727088c1e0c9631dbafcc12e0465e32
openglory <rr4l813102+github@gmail.com>
2025-02-01 02:49:28 +0530

    Update files.js

--- files.js
+++ files.js
@@ -1,7 +1,8 @@
-let folder_icon_url = https://cdn-icons-png.flaticon.com/512/3767/3767084.png
+let folder_icon_url = "https://cdn-icons-png.flaticon.com/512/3767/3767084.png"
+let folder_empty_url = "#"
 
 let file_list = [
     { filepath: "apps/periodic-table-of-elementshtml-css/index.html", title: "Priodic table 1", icon: folder_icon_url },
-    { filepath: "file_2.html", title: "File 2", icon: "icon2.png" },
-    { filepath: "file_3.html", title: "File 3", icon: "icon3.png" }
+    { filepath: folder_empty_url, title: "File 2", icon: folder_icon_url },
+    { filepath: folder_empty_url, title: "File 3", icon: folder_icon_url }
 ];
